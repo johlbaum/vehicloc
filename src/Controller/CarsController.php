48236ad0c9acc593fd99c63fbb10c3e8e5ddde14
johlbaum <julien.ohlbaum@gmail.com>
2024-07-11 19:30:20 +0200

    Added car detail page

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -9,13 +9,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CarsController extends AbstractController
 {
-    #[Route('/', name: 'app_cars')]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(CarRepository $repository): Response
     {
         $cars = $repository->findAll();
 
         return $this->render('main/index.html.twig', [
             'cars' => $cars
+        ]);
+    }
+
+    #[Route('/voiture/{id}', name: 'app_car', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(int $id, CarRepository $repository): Response
+    {
+        $car = $repository->find($id);
+
+        if (!$car) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('car/show.html.twig', [
+            'car' => $car
         ]);
     }
 }
